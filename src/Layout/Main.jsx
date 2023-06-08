54ea1ac11a4ebcd2dd0269ab08fc51c8e20ceaf2
rabby6530 <you@example.com>
2023-06-08 19:26:30 +0600

    added navbar

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/pages/Shared/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Main;
