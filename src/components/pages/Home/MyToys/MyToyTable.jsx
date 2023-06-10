577af1417373e1e67dbc0584c07a35ce0f463a01
rabby6530 <you@example.com>
2023-06-10 13:45:04 +0600

    added delete method

--- src/components/pages/Home/MyToys/MyToyTable.jsx
+++ src/components/pages/Home/MyToys/MyToyTable.jsx
@@ -1,7 +1,10 @@
+import { FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const MyToyTable = ({ toyCar }) => {
+const MyToyTable = ({ toyCar, handleDelete }) => {
     const { _id, name, sellerName, price, quantity, subCategory } = toyCar;
+
+  
 
     return (
 
@@ -13,6 +16,10 @@ const MyToyTable = ({ toyCar }) => {
             <td>{quantity}</td>
             <th>
                 <Link to={`/singleMyToys/${_id}`}> <button className="btn btn-ghost btn-xs">View</button></Link>
+            </th>
+            <th>
+                <button className="btn btn-ghost btn-xs mr-2"><FaEdit className=""></FaEdit> Edit </button>
+                <button onClick={()=> handleDelete(_id)} className="btn btn-ghost btn-xs"><span className="text-red-400">X</span></button>
 
             </th>
         </tr>
--- src/components/pages/Home/MyToys/MyToys.jsx
+++ src/components/pages/Home/MyToys/MyToys.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../../Providers/AuthProvider";
 import MyToyTable from "./MyToyTable";
+import Swal from "sweetalert2";
 
 
 const MyToys = () => {
@@ -14,43 +15,64 @@ const MyToys = () => {
             .then(data => setToycars(data))
     }, [])
 
+    const handleDelete = (id) => {
+        console.log(id)
+        fetch(`http://localhost:3000/allToy/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount>0){
+                    Swal.fire(
+                        'Deleted!',
+                        'Your Chocolate has been deleted.',
+                        'success')
+
+                    console.log(data);
+                }
+                const remaining = toyCars.filter(toyCar => toyCar._id !== id);
+                setToycars(remaining);
+            } )
+    }
+
     return (
         <div>
             <div className="my-4">
-            <h2 className="text-4xl text-bold text-center">My Toys</h2>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>
-                                Toy Name
-                            </th>
-                            <th>
-                                Seller Name
-                            </th>
-                            <th>Sub Category</th>
-                            <th>Price</th>
-                            <th>Available Quantity</th>
-                            <th>View Details</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <h2 className="text-4xl text-bold text-center">My Toys</h2>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    Toy Name
+                                </th>
+                                <th>
+                                    Seller Name
+                                </th>
+                                <th>Sub Category</th>
+                                <th>Price</th>
+                                <th>Available Quantity</th>
+                                <th>View Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
 
-                        {
-                           toyCars.map(toyCar => <MyToyTable
-                                key={toyCar._id}
-                                toyCar={toyCar}
-                            ></MyToyTable>)
-                        }
+                            {
+                                toyCars.map(toyCar => <MyToyTable
+                                    key={toyCar._id}
+                                    toyCar={toyCar}
+                                    handleDelete={ handleDelete}
+                                ></MyToyTable>)
+                            }
 
-                    </tbody>
+                        </tbody>
 
 
-                </table>
+                    </table>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
